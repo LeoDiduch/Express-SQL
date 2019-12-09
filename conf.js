c924437409c47c84e5f3e2450ce8768d442886fb
Léo <54897006+LeoDiduch@users.noreply.github.com>
2019-12-09 17:24:35 +0100

    Update conf.js

--- conf.js
+++ conf.js
@@ -3,8 +3,8 @@ const connection = mysql.createConnection({
   host: "127.0.0.1", // adresse du serveur
   port: 3306,
   user: "root", // le nom d'utilisateur
-  password: "jecodemysql", // le mot de passe
-  database: "express_sql_quest" // le nom de la base de données
+  password: "******", // le mot de passe
+  database: "ExpressQuest" // le nom de la base de données
 });
 
 module.exports = connection;
